669c51a5bc6fe34e88a023b69c6975f90e736304
pze <pze@pop-os.localdomain>
2021-12-11 01:14:54 -0300

    Menu page development

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './Global.css'
 import Login from "./Pages/Login";
 import Cadastro from "./Pages/Cadastro";
 import Contact from "./Pages/Contact";
+import { Menu } from "./Pages/Menu";
 
 function App() {
   return (
     <>
       <Main_navbar/>
+      <Menu/>
       {/* <Login /> */}
       {/* <Cadastro /> */}
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </>
   );
---
+++ src/Pages/Menu/index.js
@@ -0,0 +1,43 @@
+import Product_Container from "../../components/Product_Container";
+import {Background} from "./style";
+
+export const Menu = () => {
+    return (
+        <>
+            <Background>
+                <div class="container">
+                    <div class="titulo"> 
+                        <h1>PIZZAS SALGADAS</h1>
+                    </div>
+                    <div class="Product_Container">
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                    </div>
+
+                    <div class="titulo"> 
+                        <h1>PIZZAS DOCES</h1>
+                    </div>
+                    <div class="Product_Container">
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                        <Product_Container/>
+                    </div>
+                </div>
+                
+                
+            </Background>
+            
+        </>
+
+    );
+}
